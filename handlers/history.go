5a9fdbe06bdcb76b7e1689f2ada42e560e594ccf
Amulya Belbase <amulyabelbase@lftechnology.com>
2024-09-25 15:39:11 +0545

    fix: Empty history check added

--- handlers/history.go
+++ handlers/history.go
@@ -3,6 +3,11 @@ package handlers
 import "fmt"
 
 func ShowHistory(chatHistory []ChatHistoryOutput) {
+	if len(chatHistory) == 0 {
+		fmt.Println("No chat history found.")
+		return
+	}
+
 	for _, chat := range chatHistory {
 		username := chat.SenderInfo.Username
 		message := chat.Message
